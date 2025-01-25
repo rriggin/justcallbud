34e3dfbc52a9622c94e94868fb84a91eeacaf72e
Ryan Riggin <riggin@gmail.com>
2025-01-24 19:16:00 -0600

    adding gevent

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,4 +1,11 @@
-workers = 4
-bind = "0.0.0.0:$PORT"
-timeout = 120  # Longer timeout for LLM responses
-worker_class = 'gevent'  # For async support
+import multiprocessing
+
+max_requests = 1000
+max_requests_jitter = 50
+
+log_file = "-"
+
+bind = "0.0.0.0:10000"
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = "sync"  # Change from gevent to sync
+timeout = 120
